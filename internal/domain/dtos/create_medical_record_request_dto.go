32e23ca1fc2407b54e147f5a82593409051f8239
LesterCerioli <lesterlucasit@hotmail.com>
2025-05-25 12:08:03 -0700

    issue(#1): Updating someone details

--- internal/domain/dtos/create_medical_record_request_dto.go
+++ internal/domain/dtos/create_medical_record_request_dto.go
@@ -6,7 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-// CreateMedicalRecordRequest defines the payload for creating a new medical record.
 type CreateMedicalRecordRequest struct {
 	PatientID  uuid.UUID       `json:"patient_id" validate:"required"`
 	RecordData json.RawMessage `json:"record_data" validate:"required"` // Expecting a valid JSON object
--- internal/domain/repositories/medical_record_repository_contract.go
+++ internal/domain/repositories/medical_record_repository_contract.go
@@ -3,11 +3,11 @@ package repositories
 import (
 	"context"
 
-	"github.com/google/uuid"
 	"medical-record-service/internal/domain/entities"
+
+	"github.com/google/uuid"
 )
 
-// MedicalRecordRepositoryContract defines the interface for medical record data operations.
 type MedicalRecordRepositoryContract interface {
 	Create(ctx context.Context, record *entities.MedicalRecord) error
 	GetByID(ctx context.Context, id uuid.UUID) (*entities.MedicalRecord, error)
